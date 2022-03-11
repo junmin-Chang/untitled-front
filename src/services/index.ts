cf1aea216221e495934b6ee9b9e72934069c4574
z00mni <chjm219@kookmin.ac.kr>
2022-03-12 01:41:38 +0900

    axios response interceptor -> request interceptor

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import jwtDecode from "jwt-decode";
+import { refreshToken } from "../features/auth/authSlice";
 import authHeader from "./auth/auth-header";
 
 export const axiosPublicInstance = axios.create({
@@ -8,3 +10,26 @@ export const axiosPrivateInstance = axios.create({
   baseURL: "http://localhost:8000/api",
   headers: authHeader() as { authorization: string },
 });
+
+export const setUpInterceptor = (store: any) => {
+  axiosPrivateInstance.interceptors.request.use(
+    async (config) => {
+      const user = store.getState().auth.user;
+      if (user?.accessToken) {
+        const decodedToken: { exp: number } = jwtDecode(user.accessToken);
+        if (decodedToken.exp * 1000 < new Date().getTime()) {
+          await store.dispatch(refreshToken());
+          if (config.headers) {
+            config.headers["authorization"] = `Bearer ${
+              store.getState().auth.user.accessToken
+            }`;
+          }
+        }
+      }
+      return config;
+    },
+    (err) => {
+      return Promise.reject(err);
+    }
+  );
+};
