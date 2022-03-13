e1fdaefe3d81a18a3723e97fb5af0868bdfe31b9
z00mni <chjm219@kookmin.ac.kr>
2022-03-13 12:36:57 +0900

    tailwind css backdrop filter plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    backdropFilter: {
+      none: "none",
+      blur: "blur(20px)",
+    },
+    screen: {
+      tablet: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      laptopLg: "1280px",
+      // => @media (min-width: 1280px) { ... }
+      desktop: "1920px",
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: ["tailwindcss-filters"],
 };
