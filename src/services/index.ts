2289e9836edbb6f44a16c74f7575a7fed12e046c
z00mni <chjm219@kookmin.ac.kr>
2022-03-15 00:37:10 +0900

    axios request interceptor 오류 해결(새로 발급받은 토큰이 안 붙던 현상)

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,14 +1,12 @@
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import jwtDecode from "jwt-decode";
 import { refreshToken } from "../features/auth/authSlice";
-import authHeader from "./auth/auth-header";
 
 export const axiosPublicInstance = axios.create({
   baseURL: "http://localhost:8000/api",
 });
 export const axiosPrivateInstance = axios.create({
   baseURL: "http://localhost:8000/api",
-  headers: authHeader() as { authorization: string },
 });
 
 export const setUpInterceptor = (store: any) => {
@@ -17,9 +15,12 @@ export const setUpInterceptor = (store: any) => {
       const user = store.getState().auth.user;
       if (user?.accessToken) {
         const decodedToken: { exp: number } = jwtDecode(user.accessToken);
-        if (decodedToken.exp * 1000 < new Date().getTime()) {
-          await store.dispatch(refreshToken());
-          if (config.headers) {
+        if (config.headers) {
+          config.headers.authorization = `Bearer ${
+            store.getState().auth.user.accessToken
+          }`;
+          if (decodedToken.exp * 1000 < new Date().getTime()) {
+            await store.dispatch(refreshToken());
             config.headers.authorization = `Bearer ${
               store.getState().auth.user.accessToken
             }`;
