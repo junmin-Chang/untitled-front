8fd40cbb759cb998a1162da7f12896e89da5fd3f
z00mni <chjm219@kookmin.ac.kr>
2022-03-14 13:25:38 +0900

    remove unused module

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,3 @@
-import { Link } from "react-router-dom";
-import Button from "../components/common/Button";
-import GlossyCard from "../components/GlossyCard";
 import HomeCard from "../components/Home/HomeCard";
 
 const Home = () => {
