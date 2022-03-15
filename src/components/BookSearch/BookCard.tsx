05c9a351e4bb245faf84bc12d0356a362b19a1b3
z00mni <chjm219@kookmin.ac.kr>
2022-03-15 20:42:40 +0900

    네이밍 변경

--- src/components/BookSearch/BookCard.tsx
+++ src/components/BookSearch/BookCard.tsx
@@ -42,7 +42,7 @@ const BookCard = ({ book }: BookProps) => {
           onClick={onClickHasRead}
           className="rounded-3xl bg-green-400 py-2 px-4 text-white font-black"
         >
-          읽었어요
+          서재로
         </button>
       </div>
     </GlossyCard>
