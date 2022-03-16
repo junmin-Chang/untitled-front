86d5656808ef708cc6cd88820d68ea7ec35a0608
z00mni <chjm219@kookmin.ac.kr>
2022-03-16 23:26:01 +0900

    book, post service 추가

---
+++ src/services/post/index.ts
@@ -0,0 +1,20 @@
+import { axiosPrivateInstance, axiosPublicInstance } from "..";
+
+export const getPosts = () => {
+  return axiosPublicInstance.get("/post");
+};
+export const addPost = ({
+  isbn,
+  title,
+  content,
+}: {
+  isbn: string;
+  title: string;
+  content: string;
+}) => {
+  return axiosPrivateInstance.post(`/post`, {
+    title,
+    content,
+    bookIsbn: isbn,
+  });
+};
