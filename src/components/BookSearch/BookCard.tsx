8b280a51f8ff1bd50a9812e885ca1161f5a9eba8
z00mni <chjm219@kookmin.ac.kr>
2022-03-16 23:12:52 +0900

    독서모임 찾기 버튼 추가

--- src/components/BookSearch/BookCard.tsx
+++ src/components/BookSearch/BookCard.tsx
@@ -44,6 +44,12 @@ const BookCard = ({ book }: BookProps) => {
         >
           서재로
         </button>
+        <Link
+          to={`/community/search/${book.isbn}`}
+          className="rounded-3xl bg-blue-400 py-2 px-4 text-white font-black"
+        >
+          모임 찾기
+        </Link>
       </div>
     </GlossyCard>
   );
