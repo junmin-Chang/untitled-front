59d34326b6ade415dbddde68d3c18160c6e34c58
z00mni <chjm219@kookmin.ac.kr>
2022-03-18 19:25:15 +0900

    긴 컨텐츠 문구 substring으로 처리

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -3,7 +3,8 @@ import Grid from "../layouts/Grid";
 import GlossyCard from "../components/GlossyCard";
 import { useGetAllPostsQuery } from "../features/post/postSlice";
 import { useEffect } from "react";
-
+import format from "date-fns/format";
+import convertHtmlToText from "../utils/convertHtmlToText";
 const Community = () => {
   const { isbn } = useParams();
   const { data: posts, isLoading } = useGetAllPostsQuery(isbn ?? "");
@@ -13,7 +14,7 @@ const Community = () => {
   }, [isbn]);
   if (isLoading) return <p>Loading..</p>;
   return (
-    <div className="w-full h-full flex justify-center items-center py-10">
+    <div className="w-full h-full flex justify-center items-center p-10">
       <Grid>
         {posts.map((post: any, index: number) => (
           <GlossyCard key={index}>
@@ -21,11 +22,22 @@ const Community = () => {
               <p className="font-black text-2xl">{post.title}</p>
               <img src={post.thumbnail} alt="thumbnail" />
             </Link>
-            <p>{post.content}</p>
-            <p className="text-xs text-black-200">작성자 by.{post.userId}</p>
-            <p>{post.createdAt}</p>
+            <p className="my-4">
+              {post.content.length > 40
+                ? convertHtmlToText(post.content.substring(0, 40) + "...")
+                : convertHtmlToText(post.content)}
+            </p>
+            <div className="mt-auto">
+              <p className="inline block bg-emerald-300	rounded-xl p-1 px-2 text-white font-black">
+                작성자 by.{post.userId}
+              </p>
+              <p className="text-xs text-black-200 mt-2">
+                {format(new Date(post.createdAt), "yyyy/MM/dd")}
+              </p>
+            </div>
           </GlossyCard>
         ))}
+        {posts.length === 0 && <div>게시글이 없어요 </div>}
       </Grid>
     </div>
   );
