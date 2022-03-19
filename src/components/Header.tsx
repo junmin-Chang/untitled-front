903fbaaf536417c1a3733e3d0578dd872f85d3b6
z00mni <chjm219@kookmin.ac.kr>
2022-03-19 15:50:32 +0900

    header style to sticky

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,13 @@
 import { Link } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
-import { logout } from "../features/auth/authSlice";
 import Button from "./common/Button";
 import { FcReadingEbook } from "react-icons/fc";
+import Dropdown from "./Dropdown";
 
 const Header = () => {
   const { isLoggedIn, user } = useAppSelector((state) => state.auth);
-  const dispatch = useAppDispatch();
   return (
-    <header className="bg-white">
+    <header className="bg-white sticky top-0">
       <div className="flex px-5 py-4 border-b border-1 border-black[0.1] itmes-center justify-between">
         <Link to="/">
           <div className="flex flex-row">
@@ -25,28 +24,7 @@ const Header = () => {
                 어서오세요,{" "}
                 <b className="text-green-700">{user.user.userName} 님</b>!
               </span>
-              <Link to="/shelf/all">
-                <Button
-                  onClick={() => {}}
-                  className="bg-green-200 text-black font-medium"
-                >
-                  서재
-                </Button>
-              </Link>
-              <Link to="/book">
-                <Button
-                  onClick={() => {}}
-                  className="bg-green-300 text-black font-medium"
-                >
-                  책 찾기
-                </Button>
-              </Link>
-              <Button
-                onClick={() => dispatch(logout())}
-                className="bg-green-500 text-white font-medium"
-              >
-                로그아웃
-              </Button>
+              <Dropdown />
             </div>
           ) : (
             <div className="space-x-2.5">
